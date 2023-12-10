ee380ba3ff49d8360d84d457d0a4812dec556d39
juan <juandasa74@gmail.com>
2023-12-10 23:36:46 +0000

    Completed Extinguishing logic

--- Assets/All/FireFighter/Extinguisher.cs
+++ Assets/All/FireFighter/Extinguisher.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class Extinguisher : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    [SerializeField] private float amountExtingushedPerSecond = 1.0f;
     void Update()
     {
-        
+        if(Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out RaycastHit hit, 100f)
+        && hit.collider.TryGetComponent(out Fire fire))
+        fire.TryExtinsguish(amountExtingushedPerSecond * Time.deltaTime);
+
     }
 }
--- Assets/All/FireFighter/Fire.cs
+++ Assets/All/FireFighter/Fire.cs
@@ -4,18 +4,27 @@ using UnityEngine;
 
 public class Fire : MonoBehaviour
 {
-    //Only allow fire range to be between 0 and 1
+    //Only allows the fire intensity to be between 0 and 1
     [SerializeField, Range(0f,1f)] private float currentIntensity = 1.0f;
-    //Start all of the intensities at a determined number
+    //Array to store all the intensity values
     private float [] startIntensities = new float [0];
 
-    //Array of partivcle systems to be able to adjust all of the fire particle systems at the same time
+    //Array of particle systems to be able to adjust all of the fire particle systems at the same time
     [SerializeField] private ParticleSystem [] fireParticleSystems = new ParticleSystem[0];
+
+    //Store last watered time and intialize generation variables
+    float timeLastWatered = 0;
+    [SerializeField] private float regenDelay = 2.5f;
+    [SerializeField] private float regenRate = .1f;
+
+    private bool isLit = true;
 
     private void Start()
     {
+        //intiliase array to store fire particles system
         startIntensities = new float[fireParticleSystems.Length];
 
+        //populate array with the current emission rate of each particle
         for(int i = 0; i < fireParticleSystems.Length; i++)
         {
             startIntensities[i] = fireParticleSystems[i].emission.rateOverTime.constant;
@@ -25,14 +34,40 @@ public class Fire : MonoBehaviour
 
     private void Update()
     {
+        // If the fire is being extinsguhed and egeneration delay has passed. Start regeneration
+        if(isLit && currentIntensity < 1.0f && Time.time - timeLastWatered >= regenDelay)
+        {
+            currentIntensity += regenRate * Time.deltaTime;
+            ChangeIntensity();
+        }
+    }
+
+    //Method used to extinguish fire
+    public bool TryExtinsguish(float toReduce)
+    {
+        //Store time of extingushing 
+        timeLastWatered = Time.time;
+        //Recude the current intensity by the set amount
+        currentIntensity -= toReduce;
+
+        //Check if fire was extingushed
+        if(currentIntensity <= 0)
+        {
+            isLit = false;
+            return true;
+        }
+
         ChangeIntensity();
+
+        //fire is still lit
+        return false; 
     }
 
     private void ChangeIntensity()
     {
+        //loop over fire particle systems and adjust the rate Over time based on the current intensity
         for(int i = 0; i < fireParticleSystems.Length; i++)
         {
-            //This is to modify the emission
             var emission = fireParticleSystems[i].emission;
             emission.rateOverTime = currentIntensity * startIntensities[i];
         }
