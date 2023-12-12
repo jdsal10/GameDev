12d88bc95298f215b86d53c95907169309c4a6da
juan <juandasa74@gmail.com>
2023-12-12 18:14:35 +0000

    Water Hose  activates when player presses key "f"

--- Assets/All/FireFighter/Extinguisher.cs
+++ Assets/All/FireFighter/Extinguisher.cs
@@ -5,11 +5,24 @@ using UnityEngine;
 public class Extinguisher : MonoBehaviour
 {
     [SerializeField] private float amountExtingushedPerSecond = 1.0f;
+    public GameObject waterHose; //link to fire hose object
+
+
+    void Start()
+    {
+    }
+
     void Update()
     {
-        if(Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out RaycastHit hit, 100f)
-        && hit.collider.TryGetComponent(out Fire fire))
-        fire.TryExtinsguish(amountExtingushedPerSecond * Time.deltaTime);
-
+        //If F is pressed, shoot water
+        if(Input.GetKey(KeyCode.F)){
+            waterHose.SetActive(true);
+            if(Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out RaycastHit hit, 100f) && hit.collider.TryGetComponent(out Fire fire))
+             fire.TryExtinsguish(amountExtingushedPerSecond * Time.deltaTime);
+        }else{
+            //stop shooting water when f is not pressed
+            waterHose.SetActive(false);
+            // waterHoseParticles.Stop();
+        }
     }
 }
