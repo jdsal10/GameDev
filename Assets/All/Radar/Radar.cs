c0e0744b13d188d7f3dce439d6be4cf8cb1c0ff8
juan <juandasa74@gmail.com>
2023-12-14 05:31:39 +0000

    radar completed

--- Assets/All/Radar/Radar.cs
+++ Assets/All/Radar/Radar.cs
@@ -7,6 +7,9 @@ public class Radar : MonoBehaviour
     public GameObject[] trackedObjects;
     List<GameObject> radarObjects;
     public GameObject radarPrefab;
+    List<GameObject> borderObjects;
+    public float switchDistance;
+    public Transform helpTransform;
 
 
 
@@ -20,14 +23,34 @@ public class Radar : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        for(int i = 0; i < radarObjects.Count; i++){
+            if(Vector3.Distance(radarObjects[i].transform.position, transform.position) > switchDistance){
+                //switch to the border objects
+                helpTransform.LookAt(radarObjects[i].transform);
+                //determine correct position for the border object
+                borderObjects[i].transform.position = transform.position + switchDistance*helpTransform.forward;
+                borderObjects[i].layer = LayerMask.NameToLayer("Radar");
+                radarObjects[i].layer = LayerMask.NameToLayer("Invisible");
+            }else{
+                //switch back to the radar objects
+                borderObjects[i].layer = LayerMask.NameToLayer("Invisible");
+                radarObjects[i].layer = LayerMask.NameToLayer("Radar");
+
+            }
+        }
     }
     void createRadarObjects()
     {
         radarObjects = new List<GameObject>();
+        borderObjects = new List<GameObject>();
+
         foreach(GameObject o in trackedObjects){
+            //instantiate on radar objects
             GameObject k = Instantiate(radarPrefab, o.transform.position, Quaternion.identity) as GameObject;
             radarObjects.Add(k);
+            //Instantiate border objects
+            GameObject j = Instantiate(radarPrefab, o.transform.position, Quaternion.identity) as GameObject;
+            borderObjects.Add(j);
         }
     }
 }
