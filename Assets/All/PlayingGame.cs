644bd4478bc2a3759cd5d1b9f2c2f6082dd9cb75
krethuna3 <krethuna3@gmail.com>
2023-12-20 23:03:13 +0000

    Code Quit button

--- Assets/All/PlayingGame.cs
+++ Assets/All/PlayingGame.cs
@@ -8,17 +8,22 @@ public class PlayingGame : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void Game()
     {
         SceneManager.LoadSceneAsync(1);
+    }
+
+    public void Quit()
+    {
+        Application.Quit(); 
     }
 }
