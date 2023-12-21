eb3b55417516bcad97677261d4f1050a7bbc07ce
juan <juandasa74@gmail.com>
2023-12-21 19:07:48 +0000

    Spawner Initial

---
+++ Assets/All/FireFighter/FireSpawner.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireSpawner : MonoBehaviour
+{
+    // public GameObject firePrefab;
+    // public Transform[] spawnLocation;
+    // private int toSpawn = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnFires()
+    {
+        
+    }
+}
