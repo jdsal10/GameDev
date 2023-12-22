4d0cf425c59adc113cecef99d65393df4edabf60
krethuna3 <krethuna3@gmail.com>
2023-12-22 15:57:25 +0000

    comment my code

--- Assets/All/Car/carController.cs
+++ Assets/All/Car/carController.cs
@@ -4,30 +4,37 @@ using UnityEngine;
 
 public class carController : MonoBehaviour
 {
+    // Input axis names
     private const string Horizontal = "Horizontal";
     private const string Vertical = "Vertical";
 
+    // Input values
     private float horizontalInput;
     private float verticalInput;
+
+    // Steering and braking variables
     private float actualSteeringAngle;
     private float currentBreakingForce;
     private bool breaking;
 
+    // Motor-related parameters
     public float motorForce;
     public float breakingForce;
     public float maximumSteeringAngle;
 
+    // Wheel colliders for each wheel
     public WheelCollider flWheelCollider;
     public WheelCollider frWheelCollider;
     public WheelCollider blWheelCollider;
     public WheelCollider brWheelCollider;
 
+    // Wheel transforms for visual representation
     public Transform flWheelTransform;
     public Transform frWheelTransform;
     public Transform blWheelTransform;
     public Transform brWheelTransform;
 
-   public AudioClip carMovingAudio;
+    public AudioClip carMovingAudio;  // Audio clip for car movement sound
 
     // Start is called before the first frame update
     void Start()
@@ -38,9 +45,10 @@ public class carController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 
+    // Method to handle player input
     public void HandleInput()
     {
         GetInputOfPlayer();
@@ -49,37 +57,47 @@ public class carController : MonoBehaviour
         WheelsChange();
     }
 
+    // Method to control the motor (acceleration and braking)
     public void Motor()
     {
+        // Apply motor torque to the front wheels based on the vertical input
         flWheelCollider.motorTorque = verticalInput * motorForce;
         frWheelCollider.motorTorque = verticalInput * motorForce;
+
+        // Determine braking force based on the 'breaking' boolean
         currentBreakingForce = breaking ? breakingForce : 0f;
+
+        // Apply brakes to all wheels
         ApplyingBrekes();
 
-        AudioSource.PlayClipAtPoint(carMovingAudio, transform.position);
-
-       if (carMovingAudio != null)
+        // Play car movement audio if available
+        if (carMovingAudio != null)
         {
             AudioSource.PlayClipAtPoint(carMovingAudio, transform.position);
         }
     }
 
+    // Method to apply brakes to all wheels
     public void ApplyingBrekes()
     {
         frWheelCollider.brakeTorque = currentBreakingForce;
         flWheelCollider.brakeTorque = currentBreakingForce;
         brWheelCollider.brakeTorque = currentBreakingForce;
         blWheelCollider.brakeTorque = currentBreakingForce;
-
     }
 
+    // Method to control steering
     public void Steering()
     {
+        // Calculate the actual steering angle based on the horizontal input
         actualSteeringAngle = maximumSteeringAngle * horizontalInput;
+
+        // Apply steering angle to the front wheel colliders
         flWheelCollider.steerAngle = actualSteeringAngle;
         frWheelCollider.steerAngle = actualSteeringAngle;
     }
 
+    // Method to get player input values
     public void GetInputOfPlayer()
     {
         horizontalInput = Input.GetAxis(Horizontal);
@@ -87,6 +105,7 @@ public class carController : MonoBehaviour
         breaking = Input.GetKey(KeyCode.Space);
     }
 
+    // Method to update the visual representation of the wheels
     public void WheelsChange()
     {
         UpdateWheel(flWheelCollider, flWheelTransform);
@@ -95,11 +114,16 @@ public class carController : MonoBehaviour
         UpdateWheel(brWheelCollider, brWheelTransform);
     }
 
+    // Method to update the position and rotation of a wheel's visual representation
     public void UpdateWheel(WheelCollider wheelCollider, Transform wheelTransform)
     {
         Vector3 pos;
         Quaternion rot;
+
+        // Get the world position and rotation of the wheel collider
         wheelCollider.GetWorldPose(out pos, out rot);
+
+        // Update the visual representation's position and rotation
         wheelTransform.rotation = rot;
         wheelTransform.position = pos;
     }
--- Assets/Countdown.cs
+++ Assets/Countdown.cs
@@ -6,28 +6,32 @@ using UnityEngine.SceneManagement;
 
 public class Countdown : MonoBehaviour
 {
-    public TextMeshProUGUI countDownText;
+    public TextMeshProUGUI countDownText; // Reference to the TextMeshProUGUI that shows the count down
     public float timeLeft;
-    private int min;
-    private int sec;
+    private int min; //minutes
+    private int sec; //seconeds
 
     void Update()
     {
-        UpdateCountdown();
+        UpdateCountdown(); //calling the update count down methoed
     }
 
     public void UpdateCountdown()
     {
-        if (timeLeft > 0)
+        if (timeLeft > 0) //Check how much time is left 
         {
-            timeLeft -= Time.deltaTime;
+            timeLeft -= Time.deltaTime; // Decrease the remaining time 
 
+            // Calculate the minutes and seconds from the remaining time
             min = Mathf.FloorToInt(timeLeft / 60);
             sec = Mathf.FloorToInt(timeLeft % 60);
+            
+            // Format the minutes and seconds into a string and update the TextMeshProUGUI component
             countDownText.text = string.Format("{0:00}:{1:00}", min, sec);
         }
         else
-        {
+        { 
+            //When the count down changes the text to red
             timeLeft = 0;
             countDownText.color = Color.red;
             SceneManager.LoadScene("MainMenu");
